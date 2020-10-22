ad16dbccf65cecdbb51fc0c2ad405677cfba90a4
Bluice Zhen <bluice.zhen@gmail.com>
2020-10-22 20:26:36 +0800

    Add hello world page

--- front_src/App.vue
+++ front_src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <button @click="upload">dsdd</button>
+    <router-view :key="$route.path"></router-view>
   </div>
 </template>
 
@@ -23,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ front_src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import homepage from './views/homepage_view/index.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'homepage',
+    component: homepage
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router

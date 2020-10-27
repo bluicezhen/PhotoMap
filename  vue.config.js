5e82e3fa898a2219fa9dcdee9f08ebc3d1c78a13
Bluice Zhen <bluice.zhen@gmail.com>
2020-10-27 19:54:49 +0800

    add scss support

---  vue.config.js
+++  vue.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 
 const customConfig = {
-    chainWebpack: (config) => {
-        config.entry('app')
-            .clear()
-            .add('./front_src/main.js')
-            .end()
-    }
+  chainWebpack: (config) => {
+    config.entry('app')
+      .clear()
+      .add('./front_src/main.js')
+      .end()
+  }
 }
 
 module.exports = customConfig

304c7d2870d3fe78081525726644b6ab28519b79
Bluice Zhen <bluice.zhen@gmail.com>
2020-10-30 17:28:30 +0800

    homepage

--- front_src/views/homepage_view/index.vue
+++ front_src/views/homepage_view/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Hello World!</h1>
+  <div class="homepage-view">
+    <div class="top-bar">
+      <div class="top-bar-item">
+        <el-button size="small">upload</el-button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+@import "./index";
+</style>

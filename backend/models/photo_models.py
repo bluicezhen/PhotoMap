d11bd8f8fd9b76e6a43ce4c2df79a29afd8a468b
Bluice Zhen <bluice.zhen@gmail.com>
2020-11-12 20:43:50 +0800

    Add field file_size, user, is_upload_tencent_cos to DB model PhotoModel

--- backend/models/photo_models.py
+++ backend/models/photo_models.py
@@ -1,5 +1,9 @@
-from django.db.models import CharField, Model
+from django.db.models import BooleanField, CharField, CASCADE, ForeignKey, IntegerField, Model
+from django.contrib.auth.models import User
 
 
 class PhotoModel(Model):
     file_name = CharField(max_length=256, null=False)
+    file_size = IntegerField()
+    user = ForeignKey(User, on_delete=CASCADE)
+    is_upload_tencent_cos = BooleanField(default=False)

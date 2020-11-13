dc4d6bcfcf411e9bad133d94bf63d1ab960fbcaf
Bluice Zhen <bluice.zhen@gmail.com>
2020-11-13 17:44:31 +0800

    Add API: POST /api/photo/

--- backend/serializers/photo_serializer.py
+++ backend/serializers/photo_serializer.py
@@ -1,9 +1,19 @@
+from typing import TYPE_CHECKING
+
 from rest_framework.serializers import ModelSerializer
 
 from backend.models import PhotoModel
+
+if TYPE_CHECKING:
+    from rest_framework.request import Request
 
 
 class PhotoSerializer(ModelSerializer):
     class Meta:
         model = PhotoModel
-        fields = ['id', 'file_name']
+        fields = ['id', 'file_name', 'file_size']
+
+    def create(self, validated_data):
+        request: 'Request' = self.context.get('request')
+        validated_data['user'] = request.user
+        return super().create(validated_data)
--- backend/views/photo_view.py
+++ backend/views/photo_view.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 
@@ -7,8 +7,8 @@ from backend.models import PhotoModel
 from backend.serializers import PhotoSerializer
 
 
-class PhotoView(ListModelMixin, GenericViewSet):
+class PhotoView(CreateModelMixin, ListModelMixin, GenericViewSet):
     authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated,)
     queryset = PhotoModel.objects
     serializer_class = PhotoSerializer
